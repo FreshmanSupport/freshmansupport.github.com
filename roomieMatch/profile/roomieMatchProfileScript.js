03a2b4f20a8a613ef7c478aabf6f15f2912891a2
Chris Lai <chrislai@vt.edu>
2013-08-01 13:38:36 -0500

    Started on Budget Planner

--- roomieMatch/profile/roomieMatchProfileScript.js
+++ roomieMatch/profile/roomieMatchProfileScript.js
@@ -3,5 +3,29 @@ $(document).ready(function () {
 		   
            $('.matchDetails').toggle();
            return false;
-       });        
+       });
+	   
+	var you = $('#you');
+	var roommate = $('#roommate');
+	var importance =$('#importance');
+	//Initial Set up
+	$(roommate).hide();
+	$(importance).hide();
+	
+	$('.editNav span').click(function(e) {
+      var name = event.target.id;
+	  name = name.replace('Click', '');
+	  hideRemove();
+	  $('#'+event.target.id).addClass('select');
+	  $('#'+name).toggle();
+	  //$(par).slideToggle('slow');
+      e.preventDefault();
+  });
+  function hideRemove()
+  {
+	$('.editNav span').removeClass('select');
+	$(you).hide();  
+  	$(roommate).hide();
+	$(importance).hide();
+  }        
 })
