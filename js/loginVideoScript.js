388632dfd65f1f9bf74a25cbfa9e4706f3d3d6cc
Chris Lai <chrislai@vt.edu>
2013-08-11 12:00:09 -0500

    Video Player is Up

---
+++ js/loginVideoScript.js
@@ -0,0 +1,41 @@
+$(document).ready(function(e) {
+    $('.xClose').click(function(){
+		$('#overlay').hide();
+		$('#overlayBackground').hide();
+	});
+	$('#return').click(function(){
+		$('#overlay').hide();
+		$('#overlayBackground').hide();
+	});
+	
+	$('#loginBarContainer a').click(function(){
+		  $('#overlay').show();
+		  $('#overlayBackground').show();
+	});
+	
+	$('#overlayBackground').click(function(){
+		  $('#overlay').hide();
+		  $('#overlayBackground').hide();
+		  $('.videoOverlay').hide();
+	});
+	
+	 $('.videoOverlay .xClose').click(function(){
+		$('.videoOverlay').hide();
+		$('#overlayBackground').hide();
+	});
+	
+	//Use onclick to trigger video
+	//Not sure how to trigger multiple video on pages
+	 showVideo()
+	{
+		$('.videoOverlay').show();
+		$('#overlayBackground').show();
+	}
+	//Switch to ThankYou Screen when clicking on Register
+	$('#registerButton').click(function(){
+		$('#loginRegisterContainer').hide();
+		$('#registerThankYou').show();
+	});
+	
+	
+});
